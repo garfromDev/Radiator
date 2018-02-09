32026228f4dc4aeef0f3e6c583ed7e728a003353
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-02-09 22:15:30 +0100

    unformally tested

--- ActionSequencer.py
+++ ActionSequencer.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-import Rolling
+import threading
 
 # this object is an elementary couple action + time before next action (in sec.) to use with the Sequencer 
+# action : a function
+# duration : in sec.
 class Action:
     def __init__(self, action, duration):
         self.action = action
@@ -10,21 +12,26 @@ class Action:
         
 # the sequencer will repeat each action, then wait for duration, rolling through the sequence of Action
 class ActionSequencer:
-def __init__(self, sequence=None):
-  self.sequence = sequence
+   #sequence : a Rolling() collection of Action() 
+  def __init__(self, sequence=None):
+    self.sequence = sequence
+    self.timer = None
  
-# start the sequencer if a sequence is given, or shhot the next action (called by the timer from previous call)
-def start(self, sequence=None):
+  # start the sequencer if a sequence is given, or shhot the next action (called by the timer from previous call)
+  def start(self, sequence=None):
    if sequence != None:
-        self.timer.cancel()
+        self.cancel() #Setting a new sequence cancel the previous one if there was one
         self.sequence = sequence
-   currentAction = self.sequence.get()
-   currentAction.action()
-   self.timer = Timer(currentAction.duration, self.start)
-   self.timer.start()
+   currentAction = self.sequence.get() 
+   currentAction.action() #perform the first action
+   self.timer = threading.Timer(currentAction.duration, self.start) 
+   self.timer.start() #this will call start() again after duration, which will perform the next action
 
-# stop the sequencer        
-def cancel(self):
-   self.timer.cancel()
+  # stop the sequencer        
+  def cancel(self):
+   try: #because timer may not exist
+     self.timer.cancel()
+   except:
+     pass
    
    
