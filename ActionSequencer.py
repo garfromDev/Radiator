d24f787650dcd9a37086be3d5fde4ab960d1240e
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-02-09 21:47:07 +0100

    use start with a sequence

--- ActionSequencer.py
+++ ActionSequencer.py
@@ -10,14 +10,17 @@ class Action:
         
 # the sequencer will repeat each action, then wait for duration, rolling through the sequence of Action
 class ActionSequencer:
-def __init__(self, sequence):
+def __init__(self, sequence=None):
   self.sequence = sequence
  
-# start the sequencer 
-def start(self):
+# start the sequencer if a sequence is given, or shhot the next action (called by the timer from previous call)
+def start(self, sequence=None):
+   if sequence != None:
+        self.timer.cancel()
+        self.sequence = sequence
    currentAction = self.sequence.get()
    currentAction.action()
-   self.timer = Timer(currentAction.duration, self.shoot)
+   self.timer = Timer(currentAction.duration, self.start)
    self.timer.start()
 
 # stop the sequencer        
