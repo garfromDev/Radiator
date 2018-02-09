4be34a02c4a2ec6e8af08702a9ece2d22879709f
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-02-09 13:27:53 +0100

    Update Rolling.py
    unformally validated

--- Rolling.py
+++ Rolling.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from collections import deque
 
-class Rolling:
-   def __init__(self, liste):
-      self._list = deque()
-      
-   
+# this object manage an ordered rolling list of item, when the end is reached
+# the first item will be returned and so on
+# Return None when the collection is empty
+class Rolling(deque):
+   # return the next element of the collection
    def get(self):
-      l = liste.popleft()
-      liste.append(l)
+      if len(self) == 0:
+         return None
+      l =self.popleft()
+      self.append(l)
       return l
