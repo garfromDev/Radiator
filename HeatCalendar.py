a05596c18a56a496123239399f9a2bdb312b1dcd
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-02-12 12:47:37 +0100

    Update HeatCalendar.py

--- HeatCalendar.py
+++ HeatCalendar.py
@@ -3,10 +3,20 @@
 # javascript peut enregistrer en local des paires clef:valeur
 # PHP peut ecrire dans un fichier
 # struture json :
-# { "Monday":[
-#    { "hour":"08:00", "mode":"confort"},
-#    "08:15":"confort"
-# ] }
+# "weekCalendar":[
+#  { "day":"Monday",
+#   "modeForHour":[
+#      "08:00":"confort",
+#      "08:15":"confort"
+#    ] 
+#  }
+#  { "day":"Tuesday",
+#   "modeForHour":[
+#      "12:00":"eco",
+#      "12:15":"confort"
+#    ] 
+#  }
+# ]
 class HalfHour:
   def __init__(self, hour, metaMode):
     if hour > 48 or hour < 0 or (hour mod 2 != 0):
