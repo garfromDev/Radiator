7f79791bdaede191e341c00f7d3f87c257a594e3
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-02-19 09:45:38 +0100

    move test to test directory

--- testLibs/HeatModeLibrary.py
+++ testLibs/HeatModeLibrary.py
@@ -1,7 +1,7 @@
 import os.path
 import subprocess
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from HeatMode import HeatMode
 import RPi.GPIO as GPIO
 
