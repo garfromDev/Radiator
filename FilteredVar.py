b94b67e9494de7db0db49f06bda8eb2641213814
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-02-28 13:02:47 +0100

    code refactored

--- FilteredVar.py
+++ FilteredVar.py
@@ -9,19 +9,23 @@ class FilteredVar:
     # the function that will be called to retrieve a new value
     def __init__(self, cacheDuration, getter):
         self.cacheDuration = cacheDuration
-        self._value = getter()
-        self._valueDate = time()
         self._getter = getter
+        self._updateValue()
         
     # return the cached value if still valid (less than cacheDuration since last refresh)
     # return the new value if value is invalid
     # if getter raise exception, value will be None, and exception will be re-raised
     def value(self):
         if time() - self._valueDate > self.cacheDuration:
-            try:
-                self._value = self._getter()
-            except:
-                self._value = None
-                self._valueDate = 0 #value timestamp will be invalid
-                raise
+            self._updateValue()
         return self._value
+    
+    def _updateValue(self):
+         try:
+             self._value = self._getter()
+             self._valueDate = time()
+         except:
+             self._value = None
+             self._valueDate = 0 #value timestamp will be invalid
+             raise
+        
