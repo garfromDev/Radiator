a0e7928182efc711ec8e9d25177dc26c011dc89e
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-02-28 20:30:59 +0100

    fix bug

--- testCases/testLibs/ActionSequencerLibrary.py
+++ testCases/testLibs/ActionSequencerLibrary.py
@@ -41,6 +41,6 @@ class ActionSequencerLibrary(object):
         """
         seq=Rolling()
         for i in range(nb):
-            a = Action(self.store(lambda i=i:str(i+1)), 1) #merci http://sametmax.com/fonctions-anonymes-en-python-ou-lambda/
+            a = Action(self._store(lambda i=i:str(i+1)), 1) #merci http://sametmax.com/fonctions-anonymes-en-python-ou-lambda/
             seq.add(a)
         return seq
