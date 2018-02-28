6f25ca72e66bcaccbede470ee6bbf5c8283a92bd
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-02-28 20:35:12 +0100

    fix createSeq

--- testCases/testLibs/ActionSequencerLibrary.py
+++ testCases/testLibs/ActionSequencerLibrary.py
@@ -42,5 +42,5 @@ class ActionSequencerLibrary(object):
         seq=Rolling()
         for i in range(nb):
             a = Action(self._store(lambda i=i:str(i+1)), 1) #merci http://sametmax.com/fonctions-anonymes-en-python-ou-lambda/
-            seq.add(a)
+            seq.append(a)
         return seq
