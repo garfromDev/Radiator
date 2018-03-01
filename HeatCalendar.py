04a58b4b68a582715a158198c721b3899ce276a3
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-03-01 13:08:32 +0100

    code manual check done

--- HeatCalendar.py
+++ HeatCalendar.py
@@ -20,41 +20,36 @@
 import CST
 import time
 import json
-
-# to be deleted?
-class HalfHour:
-  def __init__(self, hour, metaMode):
-    if hour > 48 or hour < 0 or (hour mod 2 != 0):
-      raise ValueError('invalid half hour definition')
-    self.hour = hour
-    self.metaMode = metaMode
-
-    
+ 
     
 class HeatCalendar:
-
-  def setWeekCal(self, week):
-    pass
-  
+  """ This object read the calendar in a file and extract the current mode
+      The idea is to call it every 15mn to change Heating mode, the file 
+      content may be changed by the distant user via the web server
+      of course, in worst case change could take 15mn to apply
+  """
+  def __init__(self, calFile = CST.WEEKCALJSON):
+    """ :param calFile name of the json file that describe the calendar """ 
+    self._calFile = calFile
+    
   # return the meta mode for the current hour and time
   # hour and time is given by system time
   # meta mode is given by the json file defined in CST.WEEKCALJSON
   #
   def getCurrentMode(self):
     # ouvrir le fichier
-    with open(CST.WEEKCALJSON) as wcal:
+    with open( self._calFile) as wcal:
       try:
         calendar = json.load(wcal)
-        metaMode=calendar['weekCalendar'][self.day][self.hour]
+        metaMode=calendar['weekCalendar'][self.day()][self.hour()]
       except:
-        # que fait-on en cas d erreur, comment on prévient?
         #soit le fichier n'a pu être lu, soit le calendrier n'est pas complet
         metaMode="unknow"
      return metaMode
   
   # return the day in the form of 'Monday', 'Tuesday', ...
   def day(self):
-    dayNr = time.strftime(%w",localtime())
+    dayNr = int(time.strftime("%w",time.localtime())) #strftime return a string
     # we use a table conversion because locale could be different between the pi running 
     # the python programm and the app sending data to the Json file
     # so I choose the json file will be english wathever the locale language                      
@@ -62,13 +57,13 @@ class HeatCalendar:
     return dayNames[dayNr]
   
   def hour(self):
-    """ return a string with the current minute rounded to quarter
+    """ return a string with the current minute rounded to quarter 08:15
       0 to 14 -> 00
       15 to 29 -> 15
     """
-    h = time.strftime(%H",localtime()) #get the hour 00 to 23
-    m = time.strftime(%M",localtime()) #get the minute 00 to 59
-    return "%s%s" % (h, self._normalize(m))
+    h = time.strftime("%H",time.localtime()) #get the hour 00 to 23
+    m = time.strftime("%M",time.localtime()) #get the minute 00 to 59
+    return "%s:%s" % (h, self._normalize(m))
                       
   def _normalize(self, minutes):
     m = int(minutes) / 15
