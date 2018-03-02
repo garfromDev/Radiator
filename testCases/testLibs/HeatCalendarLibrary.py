4eb7e18c88394f300bf9dd5af2b969cae191063b
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-03-02 13:25:28 +0100

    ready for test

---
+++ testCases/testLibs/HeatCalendarLibrary.py
@@ -0,0 +1,20 @@
+import os.path
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from HeatCalendar import HeatCalendar
+import time
+
+class HeatCalendardLibrary(object):
+  """
+    Allows to test HeatCalendar.py with robot framework 
+  """
+  def __init__(self):
+    self._result = 'INIT'
+  
+  def set_time_to(self, testTime):
+    self._cal = HeatCalendar(localtime = lambda t=testTime: time.strptime(t, "%Y %m %d %H %M") )
+    self._result = self._cal.getCurrentMode()
+    
+  def result_should_be(self, expected):
+    if str(self._result) != expected: #arguments of robot are always string
+      raise AssertionError('%s != %s' % (self._result, expected))
