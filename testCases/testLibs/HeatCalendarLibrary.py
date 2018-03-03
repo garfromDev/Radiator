fd43217c930dea7f84b02d435e10544b1f04657e
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-03-03 10:16:14 +0100

    fix name

--- testCases/testLibs/HeatCalendarLibrary.py
+++ testCases/testLibs/HeatCalendarLibrary.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 from HeatCalendar import HeatCalendar
 import time
 
-class HeatCalendardLibrary(object):
+class HeatCalendarLibrary(object):
   """
     Allows to test HeatCalendar.py with robot framework 
   """
