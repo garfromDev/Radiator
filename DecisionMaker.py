cd3b4834ca2d1036413f392a80ba647410b97fa5
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-03-03 22:12:01 +0100

    makeDecision on going

--- DecisionMaker.py
+++ DecisionMaker.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from HeatCalendar import HeatCalendar
+from Heatmode import Heatmode
+from FilteredVar import FilteredVar
+from CurrentTemperature import InsideTemperature
+import CST
 
 class DecisionMaker(object):
   """ Central decision point of Radiator
@@ -10,4 +15,41 @@ class DecisionMaker(object):
         - sun
         - felt internal temp (combine heat and humidity)
    """
+
+  def ___init___(self):
+    self._calendar = HeatCalendar(calFile=CST.WEEKCALJSON)
+    self.metamode = FilteredVar(cacheDuration = CST.METACACHING, getter = self._calendar.getCurrentMode)
+    self._heater = Heatmode()
+    self._curTemp = InsideTemperature()
+    self.insideTemp = FilteredVar(cacheDuration = CST.TEMPCACHING, getter=self._curTemp.value)
+    self.userBonus = False #!!!! mécanisme à ajouter !!!!
+    self.userDown = False #!!!! mécanisme à ajouter !!!!
+    self.feltTemp = FilteredVar(cacheDuration = CST.TEMPCACHING, getter=self.insideTemp.value) #!!!! à remplacer !!!
+    
+    
+  def makeDecision(self):
+    #1 get meta mode
+    metaMode = self.metaMode.value()
+    
+    if metaMode != "confort":
+      self._heater.setEcoMode()
+    
+    #2 adaptation of confort mode
+    if metaMode == "confort":
+      if self.userBonus.value():
+        action = self._heater.setConfortMode
+      elif self.felTemp.value() < CST.FELT_TEMP_MIN:
+        action = self._heater.setConfortMode
+      elif self.felTemp.value() > CST.FELT_TEMP_MAX:
+        action = self._heater.setConfortModeMinus2
+      elif self.userDown.value():
+        action = self._heater.setConfortModeMinus2
+      else:
+        action = self._heater.setConfortModeMinus1
       
+      action()
+      
+    
+      
+    
+    
