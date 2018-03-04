cec48a512163691a3279b498020f2bc755dd99ce
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-03-04 21:16:05 +0100

    on going

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from ActionSequencer import Action, ActionSequencer
+from DecisionMaker import DecisionMaker
+from Rolling import Rolling
+import CST
+
+def main():
+  logging.basicConfig(filename='Radiator.log', level=logging.INFO, format='%(asctime)s %(message)s')
+  logging.info('Started')
+  decider = DecisionMaker()
+  action = Action( decider.makeDecision, CST.MAIN_TIMING)
+  mainSeq = Rolling(
