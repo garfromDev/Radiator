13e5ff3a8f279fe10434c27456ac3338b2479616
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-03-06 19:44:05 +0100

    delete FELT_TEMP_MIN

--- CST.py
+++ CST.py
@@ -6,4 +6,4 @@ WEEKCALJSON = "week.json"   #The file that defines the weekly calendar
 METACACHING = 15 * MIN      #caching duration for meta mode check
 TEMPCACHING = 2 * MIN       #caching duration for temperature check
 MAIN_TIMING  = 1 * MIN      # main looprefreshing
-FELT_TEMP_MIN
+
