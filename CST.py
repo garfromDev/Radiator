b4365c1d4d1d77a42ff6f7596392d52fe68ece7b
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-03-06 19:42:25 +0100

    ready for test

--- CST.py
+++ CST.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # This utility module define constant
-WEEKCALJSON = "week.json" #The file that defines the weekly calendar
-METACACHING = 15 * 60     #caching duration for meta mode check
-TEMPCACHING = 1 * 60      #caching duration for temperature check
-
+MIN         = 60
+WEEKCALJSON = "week.json"   #The file that defines the weekly calendar
+METACACHING = 15 * MIN      #caching duration for meta mode check
+TEMPCACHING = 2 * MIN       #caching duration for temperature check
+MAIN_TIMING  = 1 * MIN      # main looprefreshing
+FELT_TEMP_MIN
