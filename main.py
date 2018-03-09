c705283651667feb77b3dfd2cfaf42b3b5510873
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-03-09 08:16:38 +0100

    make sequencer and decider global

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from DecisionMaker import DecisionMaker
 from Rolling import Rolling
 import CST
 
-sequencer = ActionSequencer()
+sequencer = ActionSequencer() #must be global to remain alive at the end of main
 
 def main():
   logging.basicConfig(filename='Radiator.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
   logging.info('Started')
+  global decider  #must be global to remain alive at the end of main
   decider = DecisionMaker()
   action = Action( action = decider.makeDecision, duration = CST.MAIN_TIMING)
   mainSeq = Rolling([action])
