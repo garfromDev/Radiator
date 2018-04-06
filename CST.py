cd2aa2a71822fdf2493a2827f9840586debabf8c
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-04-06 08:15:07 +0200

    METACACHING -> 5mn

--- CST.py
+++ CST.py
@@ -3,7 +3,7 @@
 # This utility module define constant
 MIN         = 60
 WEEKCALJSON = "week.json"   #The file that defines the weekly calendar
-METACACHING = 15 * MIN      #caching duration for meta mode check
+METACACHING = 5 * MIN      #caching duration for meta mode check
 TEMPCACHING = 2 * MIN       #caching duration for temperature check
 MAIN_TIMING  = 1 * MIN      # main looprefreshing
 
