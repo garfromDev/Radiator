479b654c6e28db9ceb5e198850546f8b40048f1c
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-04-06 08:27:01 +0200

    MAIN_TIMING -> 15mn
    to allow time for confort minus 1

--- CST.py
+++ CST.py
@@ -5,5 +5,5 @@ MIN         = 60
 WEEKCALJSON = "week.json"   #The file that defines the weekly calendar
 METACACHING = 5 * MIN      #caching duration for meta mode check
 TEMPCACHING = 2 * MIN       #caching duration for temperature check
-MAIN_TIMING  = 1 * MIN      # main looprefreshing
+MAIN_TIMING  = 15 * MIN      # main looprefreshing
 
