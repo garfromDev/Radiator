151aaecb8a6785dd011569c66fab91271dfab2fb
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-04-08 12:12:41 +0200

    add JSON_PATH

--- CST.py
+++ CST.py
@@ -3,6 +3,7 @@
 # This utility module define constant
 MIN         = 60
 WEEKCALJSON = "week.json"   #The file that defines the weekly calendar
+JSON_PATH = "/home/pi/Program/Radiator" #the path to the weekly calendar
 METACACHING = 5 * MIN      #caching duration for meta mode check
 TEMPCACHING = 2 * MIN       #caching duration for temperature check
 MAIN_TIMING  = 15 * MIN      # main looprefreshing
