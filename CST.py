18f4606e060eb3b86a3f5640adc8212ad6e116c2
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-04-11 13:07:22 +0200

    add status.json

--- CST.py
+++ CST.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # This utility module define constant
-MIN         = 60
-WEEKCALJSON = "week.json"   #The file that defines the weekly calendar
-JSON_PATH = "/home/pi/Program/Radiator" #the path to the weekly calendar
-METACACHING = 5 * MIN      #caching duration for meta mode check
-TEMPCACHING = 2 * MIN       #caching duration for temperature check
-MAIN_TIMING  = 15 * MIN      # main looprefreshing
+MIN         = 60            # conversion of minutes to second
+WEEKCALJSON = "week.json"   # the file that defines the weekly calendar
+STATUSJSON = "status.json"  # the file that define the current status (user Down, Overrule, ..)
+JSON_PATH = "/home/pi/Program/Radiator" # the path to the weekly calendar
+METACACHING = 5 * MIN       # caching duration for meta mode check
+TEMPCACHING = 2 * MIN       # caching duration for temperature check
+MAIN_TIMING  = 15 * MIN     # main looprefreshing
 
