1a2247cc244d47c3f51558f7ab63586e5ac6d824
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-04-12 17:46:44 +0200

    change Vref + prepare filtering

--- CurrentTemperature.py
+++ CurrentTemperature.py
@@ -9,8 +9,8 @@ import Adafruit_MCP3008
 # it can be used with FilteredVar mechanism, using value() as getter
 
 class InsideTemperature:
-# sensorPin : the pin of the MPC3008 where the sensor is connected (pin 5 in my schematic)
-# voltageRef : the reference voltage of the ADC, in mV (295mV in my schematic)
+# sensorPin : the pin of the MPC3008 where the sensor is connected (pin 1 in my schematic)
+# voltageRef : the reference voltage of the ADC, in mV (581mV in my schematic)
 # adcRange : the output range of ADC converter (0 to 1023 for MCP3008)
 # sensorGain : the voltage change per degrees of the thermal sensor (10 mV / degree for LM35)
     def __init__(self, sensorPin = 1, voltageRef = 581.0,adcRange = 1024.0, sensorGain = 10.0 ):
@@ -24,9 +24,13 @@ class InsideTemperature:
         self._mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 # return the temperature value in degree, None if impossible to calculate
-# with my schematic, temperature range is 0 to 29,5 degree Celcius
-# higher temperature will be 29.5, this is not a concern for a heating regulation
+# with my schematic, temperature range is 0 to 58,1 degree Celcius
+# higher temperature will be 58,1, this is not a concern for a heating regulation
     def value(self):
+        try: #calculate the adc delta value corresponding to MAX_DELTA_TEMP temperature delta in °
+            degree = CST.MAX_DELTA_TEMP * self._sensorGain * self._adcRange / 
+        except:
+            degree = self._adcRange #no filterig will be done
         voltage = self._mcp.read_adc(self._sensorPin)
         try:
             temp = float(voltage) * (self._voltageRef / self._adcRange) / self._sensorGain
