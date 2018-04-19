07df01816d3356f6154bf63ebfed0f757c1b5feb
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-04-19 12:43:51 +0200

    Update and rename UserOverrule.py to UserInteractionManager.py

--- UserInteractionManager.py
+++ UserInteractionManager.py
@@ -2,7 +2,7 @@
 # this module allow to interface with user about
 # overruling the calendar (vacation, day at home, ...)
 # it will fetch the user decision in a json file 
-class UserOverrule:
+class UserInteractionManager:
   def __init__(userJson):
     self._jsonFile = CST.JSON_PATH + userJson
     
