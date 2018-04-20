5c7ecbd49ced22796a2cf01925a03fd822fe3fe1
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-04-20 13:36:43 +0200

    on going according architecture

--- FeltTemperature.py
+++ FeltTemperature.py
@@ -1,7 +1,46 @@
 # -*- coding: utf-8 -*-
 
 class FeltTemperature:
-  def __init__(self):
-    pass
+  """
+    this class proovide calculation of felt temperature from environmental condition
+    use : 
+    - create an object initialized with proper target temperature (real air temperature under normal condition)
+    - at init, give it access to requested information (getter function)
+    - use feltHot(), f
+    .. warning:: version provisoire non entierement fonctionnelle
+    
+  """
   
-  def feltTemperatureWith(humidity, insideTemp, outsideTemp, sunFactor
+  def __init__(self, 
+               insideTemperature,
+               outsideSunLevel,
+               insideSunLevel,
+               humidity,
+               outsideTemperature,
+               targetTemp = 19):
+    """
+      :param insideTemperature: the room temperature in Celsius
+      :param targetTemp: the target felt temperature (Celsius), similar to the air temperature without sun load under comfortable humidity
+    """
+    self.targetTemp = targetTemp
+  
+  def feltTempCold(self):
+    """
+      :return: True if user will feel cold versus target temperature
+    """
+    return False # TODO: implement!
+  
+  
+  def feltTempHot(self):
+    """
+      :return: True if user will feel hot versus target temperature
+    """
+    return False # TODO: implement!
+    
+    
+    def feltTempSuperHot(self):
+    """
+      :return: True if user will feel really hot versus target temperature
+    """
+    return False # TODO: implement!
+    
