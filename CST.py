37f35b5d694330262fc679ecca1bf52ef2e0bc63
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-08-01 22:21:16 +0200

    add _const() class definition

--- CST.py
+++ CST.py
@@ -14,3 +14,12 @@ UNKNOW = "unknow"           # unknow meta mode
 MAX_DELTA_TEMP = 0.2        # maximum temperature span between 2 simultaneous measurement for filtering (in °C)
 LM35_INTERVAL = 0.01        # interval in sec between 2 measure for filtering temperature measurement
 SUPER_HOT_DELTA = 3         # delta between target temp and measured temp to entre super hot mode
+
+class _const:
+    class ConstError(TypeError): pass
+    def __setattr__(self,name,value):
+        if self.__dict__.has_key(name):
+            raise self.ConstError, "Can't rebind const(%s)"%name
+        self.__dict__[name]=value
+import sys
+sys.modules[__name__]=_const()
