699f1242d821e074b6ddc9d6e7fcd0b0d459683b
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-11-28 13:41:31 +0100

    add const and fix bug

--- UserInteractionManager.py
+++ UserInteractionManager.py
@@ -1,40 +1,44 @@
-
-# this module allow to interface with user about
-# overruling the calendar (vacation, day at home, ...)
-# it will fetch the user decision in a json file 
-
-import CST.PY
+# -*- coding: utf-8 -*-
+import CST
+import const
+const.USER_JSON = "userInteraction.json"
+const.JSON_PATH = "/home/pi/Program/Radiator/" # the path to the weekly calendar
 
 class UserInteractionManager:
-  def __init__(userJson):
-    self._jsonFile = CST.JSON_PATH + userJson
+  """
+    this module allow to interface with user about
+    overruling the calendar (vacation, day at home, ...)
+    it will fetch the user decision in a json file 
+  """
+  def __init__(self, path=const.JSON_PATH, file=const.USER_JSON):
+    self._jsonFile = path + file
     
   def overruled(self):
     """
       return: true if user has decided to temporary overrule the heatCalendar
     """
-    return false # TODO implement!
+    return False # TODO implement!
   
   
   def overMode(self):
     """
       return: the metamode choosen by the user (ECO, CONFORT)
     """
-    return UNKNOW # TODO implement!
+    return CST.UNKNOW # TODO implement!
     
     
   def userBonus(self):
     """
       return: true if user has requested to increase temperature
     """
-    return false # TODO implement!
+    return False # TODO implement!
   
   
   def userDown(self):
     """
       return: true if user has requested to increase temperature
     """
-    return false # TODO implement! 
+    return False # TODO implement! 
 
   
   def targetTemp(self):
