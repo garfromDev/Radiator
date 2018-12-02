9e93cbe75cad83e230302d9455ba2c808dce1f01
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-12-02 10:00:48 +0100

    use const mechanism

--- CST.py
+++ CST.py
@@ -1,18 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # This utility module define constant
-MIN         = 60            # conversion of minutes to second
-WEEKCALJSON = "week.json"   # the file that defines the weekly calendar
-CALENDAR_PATH = "."         # the path to the weekly calendar file
-METACACHING = 5 * MIN       # caching duration for meta mode check
-TEMPCACHING = 2 * MIN       # caching duration for temperature check
-MAIN_TIMING  = 15 * MIN     # main looprefreshing
-CONFORT = "confort"         # confort meta mode
-ECO = "eco"                 # eco meta mode
-UNKNOW = "unknow"           # unknow meta mode
-MAX_DELTA_TEMP = 0.2        # maximum temperature span between 2 simultaneous measurement for filtering (in °C)
-LM35_INTERVAL = 0.01        # interval in sec between 2 measure for filtering temperature measurement
-SUPER_HOT_DELTA = 3         # delta between target temp and measured temp to entre super hot mode
 
 class _const:
     """
@@ -27,3 +15,17 @@ class _const:
         self.__dict__[name]=value
 import sys
 sys.modules[__name__]=_const()
+
+CST.MIN         = 60            # conversion of minutes to second
+CST.WEEKCALJSON = "week.json"   # the file that defines the weekly calendar
+CST.CALENDAR_PATH = "."         # the path to the weekly calendar file
+CST.METACACHING = 5 * MIN       # caching duration for meta mode check
+CST.TEMPCACHING = 2 * MIN       # caching duration for temperature check
+CST.MAIN_TIMING  = 15 * MIN     # main looprefreshing
+CST.CONFORT = "confort"         # confort meta mode
+CST.CONFORTPLUS = "confortPlus" # confort plus meta mode
+CST.ECO = "eco"                 # eco meta mode
+CST.UNKNOW = "unknow"           # unknow meta mode
+CST.MAX_DELTA_TEMP = 0.2        # maximum temperature span between 2 simultaneous measurement for filtering (in °C)
+CST.LM35_INTERVAL = 0.01        # interval in sec between 2 measure for filtering temperature measurement
+CST.SUPER_HOT_DELTA = 3         # delta between target temp and measured temp to entre super hot mode
