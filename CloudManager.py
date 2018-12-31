2e6ae44fbd3eac7d308c9998b354b1dcaf907e93
garfromDev <34375708+garfromDev@users.noreply.github.com>
2018-12-31 19:28:03 +0100

    ready for compile

--- CloudManager.py
+++ CloudManager.py
@@ -9,10 +9,18 @@ class CloudManager(object):
     using the DistantFileInterface
   """
   
-  def __init__(self,distantFileInterface=DistantFileInterface()):
+  def __init__(self,distantFileInterface=DistantFileInterface(), userDecisionFileName, weekCalendarFileName):
     self._distantFileInterface=distantFileInterface
     self._distantFileInterface.configure(server="")
+    self._userDecisionFileName = userDecisionFileName
+    self._weekCalendarFileName = weekCalendarFileName
+    
     
   def update(self):
-    #fetch calendar file
+    """
+      fetch the file from distant location using provided DistantFileInterface
+      file will be saved in local directory with same name, overwriting without warning
+    """  
+    self._distantFileInterface.fetch(self._weekCalendarFileName)
+    self._distantFileInterface.fetch(self._userDecisionFileName)
     
