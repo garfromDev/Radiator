13a58427c6dea2a14b1b1434707f426ba8c232d4
garfromDev <garfromDev@gmail.com>
2019-02-12 22:37:58 +0100

    WatchFile need debug

--- WatchFile.py
+++ WatchFile.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
-from os import os.path.getmtime
+from os.path import getmtime
 import threading
 import time
 import os
+import sys
 
+# this is a pointer to the module object instance itself.
+this = sys.modules[__name__]
 
 #default parameter, to be modified directly or using configure()
 delay = 30 * 60
 timeBetweenCheck = 2 * 60 * 60
 fileToWatch = None
 maxRetry = 3
+def performReboot():
+  os.system('reboot')
+
 reboot = performReboot
 
 _retryNb = 0
@@ -25,11 +31,11 @@ def configure( fileName, action = performReboot, maxRetry = 3, timeBetweenCheck 
     :param int delay: max time allowed in sec. since last modification of the file
   """
   fileToWatch = fileName
-  WatchFile.reboot = action
-  WatchFile.maxRetry = maxRetry
-  WatchFile.timeBetweenCheck = timeBetweenCheck
-  WatchFile.delay = delay
-  
+  this.reboot = action
+  this.maxRetry = maxRetry
+  this.timeBetweenCheck = timeBetweenCheck
+  this.delay = delay
+
 
 def check():
   """
@@ -39,54 +45,53 @@ def check():
   if _checkFile():
     _retryNb = 0
     return
-  #file outDated  
+  #file outDated
+  print("file outdated")
   _retryNb += 1
   if _retryNb > maxRetry:
     reboot()
-  # launch again timer for next check  
+  # launch again timer for next check
   _watchTimer.start()
-  
-  
-def performReboot():
-  os.system('reboot')
-  
-  
-def _checkFile():
-"""
-Check if last modification of fileToWatch is more recent than delay (in sec.)
 
-:return: True if last modification is more recent
-:rtype: bool
-"""
+
+def _checkFile():
+  """
+    Check if last modification of fileToWatch is more recent than delay (in sec.)
+
+    :return: True if last modification is more recent
+    :rtype: bool
+  """
+  print("checking file")
   t=0
   try:
     t = os.path.getmtime(fileToWatch)
   except: #file not accessible
     return False
   return (t - time.time()) < delay
-  
-  
+
+
 def start():
   """
-  This module will look for the last modification of the given file
-  if the time since last modified is over delay, it fails
-  if it fails more than maxRetry, rebbot will be triggered
-  
-  :return: true if parameter allows to run at least once
+    This module will look for the last modification of the given file
+    if the time since last modified is over delay, it fails
+    if it fails more than maxRetry, rebbot will be triggered
+
+    :return: true if parameter allows to run at least once
   """
   if delay == None or fileToWatch == None:
     return False
-  
+
   _watchTimer = threading.Timer(timeBetweenCheck, check)
   _watchTimer.start()
- 
- 
+
+
+
 if __name__ == '__main__':
   print("preparing manual test")
   #create dummy file
   os.system('touch testWatchFile.dummy')
-  sleep(2)
-  def sayDone:
+  time.sleep(2)
+  def sayDone():
     print("simulating reboot...")
   configure(fileName = "testWatchFile.dummy",
             action = sayDone,
@@ -95,6 +100,4 @@ if __name__ == '__main__':
             delay = 1)
   print("starting...")
   start()
-  
-    
- 
+
