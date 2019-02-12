32b2b2f6960608c043dd2cecd951e6a2bc65007b
garfromDev <34375708+garfromDev@users.noreply.github.com>
2019-02-12 21:54:22 +0100

    added comment and manual test

--- WatchFile.py
+++ WatchFile.py
@@ -15,23 +15,38 @@ reboot = performReboot
 _retryNb = 0
 _watchTimer = None
 
-def configure( fileName, action = performReboot, maxRetry = 3, timeBetween = 2*60*60, delay = 30*60):
+def configure( fileName, action = performReboot, maxRetry = 3, timeBetweenCheck = 2*60*60, delay = 30*60):
+  """
+    Configure the module
+    :param str fileName: the file to check (including path)
+    :param function action: the action to be executed if file has timed out
+    :param int maxRetry: nb of retry before triggering action
+    :param int timeBetweenCheck: time in sec. bewteen each check of the file modification date
+    :param int delay: max time allowed in sec. since last modification of the file
+  """
   fileToWatch = fileName
   WatchFile.reboot = action
   WatchFile.maxRetry = maxRetry
+  WatchFile.timeBetweenCheck = timeBetweenCheck
+  WatchFile.delay = delay
   
-  pass
 
 def check():
+  """
+    check if the file is not modified since more than delay
+    after maxRetry tentative, will trigger action
+  """
   if _checkFile():
     _retryNb = 0
     return
-    
+  #file outDated  
   _retryNb += 1
-  if _retryNb >= maxRetry:
+  if _retryNb > maxRetry:
     reboot()
-    
-
+  # launch again timer for next check  
+  _watchTimer.start()
+  
+  
 def performReboot():
   os.system('reboot')
   
@@ -41,13 +56,13 @@ def _checkFile():
 Check if last modification of fileToWatch is more recent than delay (in sec.)
 
 :return: True if last modification is more recent
+:rtype: bool
 """
   t=0
   try:
     t = os.path.getmtime(fileToWatch)
   except: #file not accessible
     return False
-  
   return (t - time.time()) < delay
   
   
@@ -66,4 +81,20 @@ def start():
   _watchTimer.start()
  
  
+if __name__ == '__main__':
+  print("preparing manual test")
+  #create dummy file
+  os.system('touch testWatchFile.dummy')
+  sleep(2)
+  def sayDone:
+    print("simulating reboot...")
+  configure(fileName = "testWatchFile.dummy",
+            action = sayDone,
+            maxRetry = 1,
+            timeBetweenCheck = 1,
+            delay = 1)
+  print("starting...")
+  start()
+  
+    
  
