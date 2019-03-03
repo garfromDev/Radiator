b4bf3707faca5929dbbaafabc0bfe72d97622ee4
garfromDev <34375708+garfromDev@users.noreply.github.com>
2019-03-03 08:58:36 +0100

    remove superhotdelta

--- CST.py
+++ CST.py
@@ -16,5 +16,4 @@ CST.ECO = "eco"                 # eco meta mode
 CST.UNKNOW = "unknow"           # unknow meta mode
 CST.MAX_DELTA_TEMP = 0.2        # maximum temperature span between 2 simultaneous measurement for filtering (in °C)
 CST.LM35_INTERVAL = 0.01        # interval in sec between 2 measure for filtering temperature measurement
-CST.SUPER_HOT_DELTA = 3         # delta between target temp and measured temp to entre super hot mode
 CST.USER_JSON = "userInteraction.json"
