1f762ae6e5709341ff64b7a5ddfbd1954602f35f
garfromDev <34375708+garfromDev@users.noreply.github.com>
2019-04-08 18:03:46 +0200

    missing to_html_from_decision_made

--- Log_to_html.py
+++ Log_to_html.py
@@ -11,6 +11,7 @@ import CST as CST
 CST.HTTP_PORT = 8000
 CST.HTML_HEADER = 'header.html'
 CST.HTML_FILE = 'index.html'  # default file served by simple HTTP server
+CST.NB_OF_LOG_LINES = 10 #nb of status log lines served as HTML
 
 """
 This module will (upon import, no configuration needed):
@@ -19,21 +20,21 @@ This module will (upon import, no configuration needed):
 - perform conversion upon timer
 """
 
-
-def convert_to_html(line_nb):
+def convert_to_html(log_file, line_nb):
     """
     The log file is made of line with input value and line with decision made,
     in debug mode there could be other lines intercaled
     """
     try:
         with open(log_file) as f:
-            with open(CST.HTML_FILE) as h:
+            with open(CST.HTML_FILE, 'w') as h:
                 back_x_lines(f, line_nb)
                 write_header(h)
-                line = find_input_value_line(f)
+                line=f.readline()
                 while line != '':  # end of file reached
+                    line = find_input_value_line(f)
                     h.write(to_html_from_input_value(line))
-                    line = find_decision_taken_kine(f)
+                    line = find_decision_taken_line(f)
                     h.write(to_html_from_decision_made(line))
     except IOError as err:
         logging.error("convert_to_html fails to convert %s to %s",
@@ -51,7 +52,7 @@ def find_input_value_line(f):
     and return it
     :param f: the file object
     :return: the line, empty string if end of line reached"""
-    inp_val = re.compile("""
+    inp_val = re.compile(r"""
         ^\S+ #début de ligne suivi de quelque chose (la date)
         \s+  #un ou des espaces
         \S+  #l'heure
@@ -61,12 +62,22 @@ def find_input_value_line(f):
         metamode
         \s+  #un ou des espaces
         =""", re.VERBOSE)
-    reached = false
+    return find_line_matching(f, inp_val)
+
+
+def find_decision_taken_line(f):
+    """ 
+    """
+    dec = re.compile(r".+Heating mode applied \:")
+    return find_line_matching(f, dec)
+
+
+def find_line_matching(f, expr):
+    reached = False
     while not reached:
         l = f.readline()
-        reached = (l == '') or inp_val.match(l)
-    return l
-
+        reached = (l == '') or expr.match(l)
+    return l      
 
 def back_x_lines(f, lines):
     f.readline()
@@ -89,16 +100,17 @@ def back_one_line(f):
 def to_html_from_input_value(line):
     dict = get_dict_from_log_line(line)
     output = "<p>"
-    if dict['feltCold']:
+    print("Dictionaire from log ------------>  ", dict)
+    if dict['feltcold']:
         dict['felt'] = 'cold'
-    if dict['feltHot']:
+    if dict['felthot']:
         dict['felt'] = 'hot'
-    if dict['feltSuperHot']:
+    if dict['feltsuperhot']:
         dict['felt'] = 'super hot'
     if dict['overruled']:
-        if dict['Bonus']:
+        if dict['bonus']:
             dict['user_action'] = 'user cold'
-        elif dict['userDown']:
+        elif dict['userdown']:
             dict['user_action'] = 'user hot'
         else:
             dict['user_action'] = dict['overMode']
@@ -112,14 +124,18 @@ def to_html_from_input_value(line):
 
 
 def get_dict_from_log_line(line):
-    pass
+    r = re.compile(r"[\S]+ = [\S]+")
+    k = [x.split()[0].lower() for x in r.findall(line)] #all keys are lowered
+    v = [x.split()[2] for x in r.findall(line)]
+    d = dict(zip(k,v))
+    return d
 
 
 """
 line = "2019-03-09 11:47:37,962 makeDecision metamode = confort temp = 20.1
 Bonus = False feltCold = False feltHot = True feltSuperHot = False userDown = False overruled = False overMode = confort"
 import re
-r = re.compile("[\S]+ = [\S]+")
+r = re.compile(r"[\S]+ = [\S]+")
 print(r.findall(line))
 k = [ x.split()[0] for x in r.findall(line) ]
 v = [ x.split()[2] for x in r.findall(line) ]
@@ -127,7 +143,9 @@ d = dict(zip(k,v))
 print(d)
 """
 
-
+def update_html():
+    convert_to_html(lofile=CST.LOG_FILE, line_nb=CST.NB_OF_LOG_LINES)
+    
 def start_generating():
     update_html()
     threading.timer(CST.MAIN_TIMING, start_generating).start()
