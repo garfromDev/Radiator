5af2edd9bc5c4916f343c341f9f63733c5e10c3c
garfromDev <34375708+garfromDev@users.noreply.github.com>
2019-04-11 13:46:29 +0200

    works, need to use table for readability

--- Log_to_html.py
+++ Log_to_html.py
@@ -8,8 +8,10 @@ import threading
 
 import CST as CST
 
+
 CST.HTTP_PORT = 8000
 CST.HTML_HEADER = 'header.html'
+CST.HTML_FOOTER = 'footer.html'
 CST.HTML_FILE = 'index.html'  # default file served by simple HTTP server
 CST.NB_OF_LOG_LINES = 10 #nb of status log lines served as HTML
 
@@ -29,29 +31,30 @@ def convert_to_html(log_file, line_nb):
         with open(log_file) as f:
             with open(CST.HTML_FILE, 'w') as h:
                 back_x_lines(f, line_nb)
-                write_header(h)
+                write_header(header=CST.HTML_HEADER, to=h)
                 line=f.readline()
                 while line != '':  # end of file reached
                     line = find_input_value_line(f)
                     h.write(to_html_from_input_value(line))
                     line = find_decision_taken_line(f)
-                    h.write(to_html_from_decision_made(line))
+                    h.write(to_html_from_decision_made(line)+"\n")
+                write_header(header=CST.HTML_FOOTER, to=h)
     except IOError as err:
         logging.error("convert_to_html fails to convert %s to %s",
                       log_file,
                       CST.HTML_FILE)
 
 
-def write_header(html_file):
-    with open(CST.HTML_HEADER) as hdr:
-        html_file.write(hdr.read())
+def write_header(header, to):
+    with open(header) as hdr:
+        to.write(hdr.read())
 
 
 def find_input_value_line(f):
     """find the next line in file f that match the expression
     and return it
     :param f: the file object
-    :return: the line, empty string if end of line reached"""
+    :return: the line, empty string if end of file reached"""
     inp_val = re.compile(r"""
         ^\S+ #début de ligne suivi de quelque chose (la date)
         \s+  #un ou des espaces
@@ -66,7 +69,8 @@ def find_input_value_line(f):
 
 
 def find_decision_taken_line(f):
-    """ 
+    """ find the line explaining which decision has been taken
+    :return: the line, empty string if end of file reached
     """
     dec = re.compile(r".+Heating mode applied \:")
     return find_line_matching(f, dec)
@@ -75,7 +79,7 @@ def find_decision_taken_line(f):
 def find_line_matching(f, expr):
     reached = False
     while not reached:
-        l = f.readline()
+        l = f.readline() #empty string if EOF
         reached = (l == '') or expr.match(l)
     return l      
 
@@ -100,6 +104,8 @@ def back_one_line(f):
 def to_html_from_input_value(line):
     dict = get_dict_from_log_line(line)
     output = "<p>"
+    if not dict:
+      return output
     print("Dictionaire from log ------------>  ", dict)
     if dict['feltcold']:
         dict['felt'] = 'cold'
@@ -118,9 +124,16 @@ def to_html_from_input_value(line):
                    ('temp', 4),
                    ('light', 5),
                    ('user_action', 10),
-                   ('felt', 4)]:
+                   ('felt', 6)]:
         output += "  " + format(dict[k], "<{}".format(pad))
-    return output + "</p>"
+    return output
+
+
+def to_html_from_decision_made(line):
+    mode_exp = re.compile(r"applied : (\w+)")
+    found = mode_exp.search(line)
+    mode = found.groups()[0] if found else ""
+    return format(mode, "<{}".format(8)) + "</p>"
 
 
 def get_dict_from_log_line(line):
@@ -128,7 +141,7 @@ def get_dict_from_log_line(line):
     k = [x.split()[0].lower() for x in r.findall(line)] #all keys are lowered
     v = [x.split()[2] for x in r.findall(line)]
     d = dict(zip(k,v))
-    return d
+    return d if len(d)>1 else None
 
 
 """
@@ -144,7 +157,7 @@ print(d)
 """
 
 def update_html():
-    convert_to_html(lofile=CST.LOG_FILE, line_nb=CST.NB_OF_LOG_LINES)
+    convert_to_html(log_file=CST.LOG_FILE, line_nb=CST.NB_OF_LOG_LINES)
     
 def start_generating():
     update_html()
@@ -156,3 +169,7 @@ Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 httpd = SocketServer.TCPServer(("", CST.HTTP_PORT), Handler)
 logging.info("starting HTTP server on port %s", CST.HTTP_PORT)
 httpd.serve_forever()
+
+""" idea to have graph served through HTML :
+https://pythonspot.com/flask-and-great-looking-charts-using-chart-js/
+"""
