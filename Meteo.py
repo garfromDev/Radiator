69d336dc615342ca5995ca85816f9c930ae7672b
garfromDev <34375708+garfromDev@users.noreply.github.com>
2019-05-13 13:56:01 +0200

    drafted interface

---
+++ Meteo.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+""" Meteo provides acess to distant meteo service
+    Initialisation of connexion is done at module importation
+"""
+
+import logging
+import CST as CST #we must import the module, not import CST from CST, to get accesss to constant defined in other modules
+import Sun_levels
+
+class Meteo(object):
+    def outside_humidity(self):
+    """ :return: current outside humidity percentage, from 0 to 100 """
+        return 50 #FIXME : temporary stub
+
+    def outside_temperature(self):
+    """ :return: current outside temperature in celsius grade """
+        return 20  #FIXME : temporary stub
+        
+    def outside_sun_level(self):
+    """ :return: current outside sun level, SUN_NONE, SUN_LOW, SUN"
+        return CST.SUN_NONE #FIXME : temporary stub
