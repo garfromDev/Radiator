ebfd772127e4ab032e3e042c3880d1c962228f6f
garfromDev <garfromdev@gmail.com>
2020-03-29 11:29:10 +0200

    wip test decision maker unitest

---
+++ test_decisionMaker.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from DecisionMaker import DecisionMaker
+
+
+class TestDecisionMaker(TestCase):
+    def setUp(self):
+        d= DecisionMaker()
+
+    def test_makeDecision(self):
+        self.fail()
